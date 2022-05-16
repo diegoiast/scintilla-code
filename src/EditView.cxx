25f21cf3b513e1c22a16e761cece909876b59596
Neil <nyamatongwe@gmail.com>
2022-05-17 09:36:45 +1000

    Fix pink selection background when printing by making it completely transparent.

--- src/EditView.cxx
+++ src/EditView.cxx
@@ -2696,6 +2696,8 @@ Sci::Position EditView::FormatRange(bool draw, CharacterRangeFull chrg, Rectangl
 	vsPrint.selection.visible = false;
 	vsPrint.elementColours.clear();
 	vsPrint.elementBaseColours.clear();
+	// Transparent:
+	vsPrint.elementBaseColours[Element::SelectionBack] = ColourRGBA(0xc0, 0xc0, 0xc0, 0x0);
 	vsPrint.caretLine.alwaysShow = false;
 	// Don't highlight matching braces using indicators
 	vsPrint.braceHighlightIndicatorSet = false;
